3c167a47b3c0ef0470492c82a5f52df7051ac362
unknown <nanukegamer@gmail.com>
2024-05-17 09:23:25 -0300

    mais funções

--- Dengue.py
+++ Dengue.py
@@ -1,33 +1,40 @@
 import csv
 import os
+from datetime import datetime, timedelta
 
-linha=0
+def tabela(matriz_inicial):
+    from tabulate import tabulate
+    tabela = tabulate(matriz_inicial, headers="firstrow", tablefmt="grid")
+    return tabela
 
 
-def ler():
-    arquivo_csv=("DadosDengue.csv")
-    with open("DadosDengue.csv","r",) as arquivo_csv:
-        leitor_csv=csv.reader(arquivo_csv,delimiter=",")
-        for linha in leitor_csv:
-            print(linha)
+
+#def ler():
+#    arquivo_csv=("DadosDengue.csv")
+#    with open("DadosDengue.csv","r",) as arquivo_csv:
+#        leitor_csv=csv.reader(arquivo_csv,delimiter=",")
+#        for linha in leitor_csv:
+#            print(linha)
             
 
-def alterar():
-    arquivo_csv=("DadosDengue.csv")
-    dados=[arquivo_csv]
-        # with open("DadosDengue.csv","w") as arquivo_csv:
-        #     editor_csv=csv.writer(arquivo_csv,delimiter="")
-
-
-def adicionar():
-    arquivo_csv=("DadosDengue.csv")
+#def alterar(matriz):
+    
+def matriz_inicial():
     with open("DadosDengue.csv","r",) as arquivo_csv:
         leitor_csv=csv.reader(arquivo_csv,delimiter=",")
-        for linha in leitor_csv:
-            lista_csv.append(linha)
+        lista_csv=[]
+        for item in leitor_csv:
+            lista_csv.append(item)
+        return lista_csv
+
+
+def adicionar(matriz):
+    for item in matriz:
+        print(item)
+    
             
 
-def ler_por_data():
+def ler_por_data(matriz):
     data=int(input("Digite a data que deseja verificar"))
     if linha[0] == data:
         print(linha)
@@ -39,7 +46,7 @@ while Menu_principal!=3:
     print(os.getcwd()) 
     Menu_principal=int(input("[1]Informações sobre a dengue\n[2]Cadastro\n[3]Sair\nOpção:"))
     if Menu_principal==1:
-        ler()    
+        adicionar(matriz_inicial())
     elif Menu_principal==2:
-        alterar()
+        adicionar()
 print('Fechando programa')
